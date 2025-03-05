b5decd7ef1fdddcebd2239b77f0bfc94080e2251
0baydullah <obaydullah.onnorokom@gmail.com>
2025-03-05 15:38:42 +0600

    welcome message edited

--- ProjectManagementTool/BusinessLogicLayer/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
+++ ProjectManagementTool/BusinessLogicLayer/obj/Debug/net8.0/BusinessLogicLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d774e8f1993df8a214112cc6f11285a9c20d4b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0994f89287c470e6bd753d5fb8a5406a22c190b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
