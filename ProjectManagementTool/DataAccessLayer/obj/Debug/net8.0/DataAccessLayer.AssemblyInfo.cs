a24548bb7b68f6c4380bd2c0ca22fa2bad76c50f
0baydullah <obaydullah.onnorokom@gmail.com>
2025-03-06 14:44:37 +0600

    connection string fix

--- ProjectManagementTool/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ ProjectManagementTool/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+851268ef899a4c0700431ca74b64a00735be514b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41bbc1e89343fb767a4019fa8c702b7e7195f66")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
