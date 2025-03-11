2e195f2fdfbf1f371558c529e8e47e1cc5c663e3
0baydullah <obaydullah.onnorokom@gmail.com>
2025-03-11 10:19:59 +0600

    ITasksService added

---
+++ ProjectManagementTool/BusinessLogicLayer/IService/ITasksService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Models.Entity;
+
+namespace BusinessLogicLayer.IService
+{
+    public interface ITasksService
+    {
+        public void AddTasks(Tasks tasks);
+        public void UpdateTasks(Tasks tasks);
+        public void DeleteTasks(Tasks tasks);
+        public Tasks GetTasks(int id);
+        public List<Tasks> GetAllTasks();
+    }
+}
