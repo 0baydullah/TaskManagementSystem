71c8a6b489dac26afce346d7ec573238ec00f364
Arif <arif24.onnorokom@gmail.com>
2025-03-12 14:27:10 +0600

    FeatureVM view model and create page created

--- ProjectManagementTool/ProjectManagementTool/Controllers/FeatureController.cs
+++ ProjectManagementTool/ProjectManagementTool/Controllers/FeatureController.cs
@@ -1,10 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLogicLayer.IService;
+using DataAccessLayer.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ProjectManagementTool.Controllers
 {
     public class FeatureController : Controller
     {
-        public IActionResult Index()
+        private readonly IMemberService _memberService;
+        public FeatureController(IMemberService memberService) 
+        { 
+            _memberService = memberService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var members = _memberService.GetAllMember();
+            // var releases = _releaseService.GetAllRelease();
+            ViewBag.Members = new SelectList(members, "MemberId", "Name");
+            //ViewBag.Releasea = new SelectList(members, "ReleaseId", "Name");
+            ViewBag.Releases = new SelectList(new List<SelectListItem>
+            {
+                new SelectListItem { Value = "1", Text = "Release-1" },
+                new SelectListItem { Value = "2", Text = "Release-2" }
+            }, "Value", "Text");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(FeatureVM featureVM)
         {
             return View();
         }
