e2947cecc4816fa40c27383dc91064754b28385a
2024 Arif <arif24.onnorokom@gmail.com>
2025-03-16 04:34:58 +0000

    Update Release.cs

--- ProjectManagementTool/DataAccessLayer/Models/Entity/Release.cs
+++ ProjectManagementTool/DataAccessLayer/Models/Entity/Release.cs
@@ -1,1 +1,31 @@
-﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models.Entity
+{
+    public class Release
+    {
+        [Key]
+        public int ReleaseId { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+
+        [Required]
+        public string? ReleaseName { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+    }
+}
