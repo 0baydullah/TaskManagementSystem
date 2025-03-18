390fbe2e1d0fc87ae2bca4f1b463ce6302de15fb
0baydullah <obaydullah.onnorokom@gmail.com>
2025-03-18 11:37:23 +0600

    Priority repository added

---
+++ ProjectManagementTool/DataAccessLayer/IRepository/IPriorityRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Models.Entity;
+
+namespace DataAccessLayer.IRepository
+{
+    public interface IPriorityRepo
+    {
+        List<Status> GetAllPriorities();
+        Status GetPriorityById(int id);
+        void AddPriority(Status status);
+        void UpdatePriority(Status status);
+        void DeletePriority(int id);
+    }
+}
