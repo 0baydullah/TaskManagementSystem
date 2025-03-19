e83cc6f01b88efd50a3d7b06eb8e5f2de93b6a20
Arif <arif24.onnorokom@gmail.com>
2025-03-19 10:45:18 +0600

    added try catch block in feature repo

--- ProjectManagementTool/DataAccessLayer/Repository/FeatureRepo.cs
+++ ProjectManagementTool/DataAccessLayer/Repository/FeatureRepo.cs
@@ -21,87 +21,129 @@ namespace DataAccessLayer.Repository
         }
         public async Task<bool> CreateFeature(Feature feature)
         {
-            var existFeature = await _context.Features.FirstOrDefaultAsync(f => f.Name.Equals(feature.Name) && f.ProjectId == feature.ProjectId);
+            try
+            {
+                var existFeature = await _context.Features.FirstOrDefaultAsync(f => f.Name.Equals(feature.Name) && f.ProjectId == feature.ProjectId);
 
-            if (existFeature == null)
-            {
-                _context.Features.Add(feature);
-                await _context.SaveChangesAsync();
-                return true;
+                if (existFeature == null)
+                {
+                    _context.Features.Add(feature);
+                    await _context.SaveChangesAsync();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
-            else
+            catch (Exception)
             {
-                return false;
-            }  
+                throw;
+            }
         }
 
         public async Task<List<FeatureWithMemberReleaseVM>> GetAllFeature(int projectId)
         {
-            var features = await _context.Features.Where(feature => feature.ProjectId == projectId).Join(_context.Members, f => f.MemberId, m => m.MemberId, (f, m) => new
+            try
             {
-                FeatureId = f.FeatureId,
-                FeatureName = f.Name,
-                Description = f.Description,
-                EstimatedPoint = f.EstimatedPoint,
-                MemberId = f.MemberId,
-                ProjectId = f.ProjectId,
-                ReleaseId = f.ReleaseId,
-                Tag = f.Tag,
-                Email = m.Email
-            }).Join(_context.Users, f => f.Email, u => u.Email, (f, u) => new
-            {
-                FeatureId = f.FeatureId,
-                FeatureName = f.FeatureName,
-                Description = f.Description,
-                EstimatedPoint = f.EstimatedPoint,
-                MemberId = f.MemberId,
-                ProjectId = f.ProjectId,
-                ReleaseId = f.ReleaseId,
-                Tag = f.Tag,
-                MemberName = u.Name,
-
-            }).GroupJoin(_context.Releases, f => f.ReleaseId, r => r.ReleaseId, (f, r) => new { f, r }).SelectMany(
-                x => x.r.DefaultIfEmpty(), (x, r) => new FeatureWithMemberReleaseVM
+                var features = await _context.Features.Where(feature => feature.ProjectId == projectId).Join(_context.Members, f => f.MemberId, m => m.MemberId, (f, m) => new
                 {
-                    FeatureId = x.f.FeatureId,
-                    Name = x.f.FeatureName,
-                    Description = x.f.Description,
-                    EstimatedPoint = x.f.EstimatedPoint,
-                    ReleaseId = x.f.ReleaseId,
-                    MemberId = x.f.MemberId,
-                    ProjectId = x.f.ProjectId,
-                    Tag = x.f.Tag,
-                    MemberName = x.f.MemberName,
-                    ReleaseName = r != null ? r.ReleaseName : "No release"
-                }).ToListAsync();
+                    FeatureId = f.FeatureId,
+                    FeatureName = f.Name,
+                    Description = f.Description,
+                    EstimatedPoint = f.EstimatedPoint,
+                    MemberId = f.MemberId,
+                    ProjectId = f.ProjectId,
+                    ReleaseId = f.ReleaseId,
+                    Tag = f.Tag,
+                    Email = m.Email
+                }).Join(_context.Users, f => f.Email, u => u.Email, (f, u) => new
+                {
+                    FeatureId = f.FeatureId,
+                    FeatureName = f.FeatureName,
+                    Description = f.Description,
+                    EstimatedPoint = f.EstimatedPoint,
+                    MemberId = f.MemberId,
+                    ProjectId = f.ProjectId,
+                    ReleaseId = f.ReleaseId,
+                    Tag = f.Tag,
+                    MemberName = u.Name,
 
-            return features;
+                }).GroupJoin(_context.Releases, f => f.ReleaseId, r => r.ReleaseId, (f, r) => new { f, r }).SelectMany(
+                    x => x.r.DefaultIfEmpty(), (x, r) => new FeatureWithMemberReleaseVM
+                    {
+                        FeatureId = x.f.FeatureId,
+                        Name = x.f.FeatureName,
+                        Description = x.f.Description,
+                        EstimatedPoint = x.f.EstimatedPoint,
+                        ReleaseId = x.f.ReleaseId,
+                        MemberId = x.f.MemberId,
+                        ProjectId = x.f.ProjectId,
+                        Tag = x.f.Tag,
+                        MemberName = x.f.MemberName,
+                        ReleaseName = r != null ? r.ReleaseName : "No release"
+                    }).ToListAsync();
+
+                return features;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
         }
 
         public async Task<Feature> GetFeatureById(int id)
         {
-            var feature = await _context.Features.FirstOrDefaultAsync(f => f.FeatureId.Equals(id));
-            return feature;
+            try
+            {
+                var feature = await _context.Features.FirstOrDefaultAsync(f => f.FeatureId.Equals(id));
+                return feature;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public async Task<Feature> GetFeatureByName(string name, int id, int projectId) 
         {
-            var feature = await _context.Features.FirstOrDefaultAsync(f => f.Name == name && f.FeatureId != id && f.ProjectId == projectId);
-            return feature;
+            try
+            {
+                var feature = await _context.Features.FirstOrDefaultAsync(f => f.Name == name && f.FeatureId != id && f.ProjectId == projectId);
+                return feature;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public async Task<bool> UpdateFeature(Feature feature)
         {
-              _context.Features.Update(feature);
-              await _context.SaveChangesAsync();
-              return true;
+            try
+            {
+                _context.Features.Update(feature);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public async Task<bool> DeleteFeature(Feature feature)
         {
-            _context.Features.Remove(feature);
-            await _context.SaveChangesAsync();
-            return true;
+            try
+            {
+                _context.Features.Remove(feature);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
