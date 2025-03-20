f7ef2347685301766408dfa031c497a79de12434
0baydullah <obaydullah.onnorokom@gmail.com>
2025-03-20 10:00:40 +0600

    test commit

---
+++ ProjectManagementTool/DataAccessLayer/Models/ViewModel/UserStoryListVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Models.Entity;
+
+namespace DataAccessLayer.Models.ViewModel
+{
+    public class UserStoryListVM
+    {
+        public List<UserStory> UserStories { get; set; }
+        public Dictionary<int,string> StatusList { get; set; }
+        public Dictionary<int,string> PriorityList { get; set; }
+        public Dictionary<int,string> CategoryList { set; get; }
+        public Dictionary<int,string> MemberList { get; set; }
+    }
+}
