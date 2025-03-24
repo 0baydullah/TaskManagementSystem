423dab46555a368f65c4efe89fc357c756bcfda2
0baydullah <obaydullah.onnorokom@gmail.com>
2025-03-24 10:21:34 +0600

    setting controller added and setting added for status, priority category

---
+++ ProjectManagementTool/ProjectManagementTool/Controllers/SettingsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManagementTool.Controllers
+{
+    public class SettingsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
