a721f43773de363eb0670e4bec319cf057a08db1
0baydullah <obaydullah.onnorokom@gmail.com>
2025-03-25 11:45:54 +0600

    bug service added

--- ProjectManagementTool/BusinessLogicLayer/IService/IBugService.cs
+++ ProjectManagementTool/BusinessLogicLayer/IService/IBugService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccessLayer.Models.Entity;
 
 namespace BusinessLogicLayer.IService
 {
-    internal interface IBugService
+    public interface IBugService
     {
+        public void AddBug(Bug bug);
+        public void UpdateBug(Bug bug);
+        public void DeleteBug(Bug bug);
+        public Bug GetBug(int id);
+        public List<Bug> GetAllBugOfStory(int id);
     }
 }
--- ProjectManagementTool/BusinessLogicLayer/Service/BugService.cs
+++ ProjectManagementTool/BusinessLogicLayer/Service/BugService.cs
@@ -3,10 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogicLayer.IService;
+using DataAccessLayer.Models.Entity;
+using log4net;
 
 namespace BusinessLogicLayer.Service
 {
-    internal class BugService
+    public class BugService : IBugService
     {
+        private readonly IBugService _bugService;
+        private readonly ILog _log = LogManager.GetLogger(typeof(BugService));
+        public BugService(IBugService bugService)
+        {
+            _bugService = bugService;
+        }
+        public void AddBug(Bug bug)
+        {
+            try
+            {
+                _bugService.AddBug(bug);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message);
+                throw ;
+            }
+        }
+
+        public void DeleteBug(Bug bug)
+        {
+            try
+            {
+                _bugService.DeleteBug(bug);
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message);
+                throw;
+            }
+        }
+
+        public List<Bug> GetAllBugOfStory(int id)
+        {
+            try
+            {
+                var bugs = _bugService.GetAllBugOfStory(id);
+                return bugs;
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message);
+                throw;
+            }
+        }
+
+        public Bug GetBug(int id)
+        {
+            try
+            {
+                var bug = _bugService.GetBug(id);
+                return bug;
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex.Message);
+                throw;
+            }
+        }
+
+        public void UpdateBug(Bug bug)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
