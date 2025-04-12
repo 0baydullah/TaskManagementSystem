b5353111fb04ad0e7e44f91153d1ea96bab9ec66
0baydullah <obaydullah.onnorokom@gmail.com>
2025-04-12 12:19:10 +0600

    my bugs section added

---
+++ ProjectManagementTool/DataAccessLayer/Models/ViewModel/TodoBugVM.cs
@@ -0,0 +1,11 @@
+﻿using DataAccessLayer.Models.Entity;
+
+namespace DataAccessLayer.Models.ViewModel
+{
+    public class TodoBugVM
+    {
+        public List<Bug> Bug { get; set; } = new List<Bug>();
+        public Dictionary<int, string> PriorityList { get; set; }
+        public Dictionary<int, string> StatusList { get; set; }
+    }
+}
