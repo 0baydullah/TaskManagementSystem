121c3d0c956039c1657e698fcdcb15824efade9e
0baydullah <obaydullah.onnorokom@gmail.com>
2025-04-23 11:33:01 +0600

    basetest modified

--- ProjectManagementTool/BusinessLogicLayer.Test/Service/BaseTest.cs
+++ ProjectManagementTool/BusinessLogicLayer.Test/Service/BaseTest.cs
@@ -10,7 +10,7 @@ namespace BusinessLogicLayer.Test.Service
         {
             // Initialize the test setup here
         }
-        public void Dispose()
+        public virtual void Dispose()
         {
             // Clean up resources here
         }
